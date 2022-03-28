ab260eb36cd3f3470715dd1ac9cb27bb5fa7db44
Oliver Lohse <56726226+Oliver-Lohse@users.noreply.github.com>
2022-03-28 14:39:51 +0200

    Create template-default.js

---
+++ template/template-default.js
@@ -0,0 +1,46 @@
+var template = 
+	"<div class='jumbotron jumbotron-fluid' style='box-shadow: 0px 0px 50px #999999;'>"
+  	+"<div class='container'>"
+    +"<h1 class='display-4'>"
+    +"    <div id='{Ueberschrift}'></div>"
+    +"</h1>"
+    +"<p class='lead'>"
+    +"    <div id='{Anleser}'></div>"
+    +"</p>"
+  	+"</div>"
+	+"</div>"
+
+	+"<div class='container'>"
+	
+	+"	<div class='row'>"
+	
+	+"    <div class='col-sm-3'>"
+	
+	+"			<ul class='nav nav-tabs nav-fill mb-5 mt-5'>"
+    +"			<li class='nav-item nav-link'></li>"
+    +"			<li class='nav-item nav-link active'>Sidebar Men&uuml;</li>"
+    +"			<li class='nav-item nav-link'></li>"
+    +"			</ul>"
+	
+	+"        	<div id='{Menue}'></div>"
+	
+	+"			<ul class='nav nav-tabs nav-fill mb-5 mt-5'>"
+    +"			<li class='nav-item nav-link'></li>"
+    +"			<li class='nav-item nav-link active'>Template Men&uuml;</li>"
+    +"			<li class='nav-item nav-link'></li>"
+    +"			</ul>"
+	
+	+"        <a href='https://getBootstrap.com'>Link </a><br>"
+	+"        <a href='http://cmsworkbench.de'  >Link  </a>"
+	+"    </div>"
+	
+	+"    <div class='col-sm-9'>"
+	+"        <div id='{Text}' class='mt-5'></div>"
+	+"    </div>"
+	
+	+"	</div>"
+	
+	+"			<hr class='mt-5'>"
+	+"<p><div id='{Fusszeile}'></div></p>"
+	
+	+"</div>";
