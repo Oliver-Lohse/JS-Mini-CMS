367ff547cdd74eb93b88cf0dd8a497bf3f4b2339
Oliver Lohse <56726226+Oliver-Lohse@users.noreply.github.com>
2022-03-28 14:42:23 +0200

    Create bausteine.js

---
+++ bausteine/bausteine.js
@@ -0,0 +1,25 @@
+<!-- ------------------------------------------------------------------------------------------------------------- -->
+<!-- Bausteine werden im CSS besonders formatiert, sie erhalten IMMER ein Box. Dies hat mehrere Vorteile.          -->
+<!-- Zum einen ist für den Redakteur von aussen ersichtlich, das es sich bei diesem Text um einen Baustein         -->
+<!-- handelt und er weiß wor er diesen für die Anpassung im Backend zu finden hat. Zum zweiten haben übergreifende -->
+<!-- Bausteine eine größere Wertigkeit, da sie in verschiedenen HTML-Seiten gleichsam Verwendung finden.           -->
+<!-- ------------------------------------------------------------------------------------------------------------- -->
+
+var bst_001_format = 
+	"<div class='alert alert-primary' role='alert'>Bausteine sind globale Texte, die zentral gepflegt und in \
+	verschiedenen HTML-Dokumenten verwendet werden sollen. Durch diese besondere Wertigkeit, erhalten sie eine \
+	besondere Formatierung und sind dadurch sofort als solche erkennbar.</div>";
+	
+var bst_002_jsneeded = 
+	"<div class='alert alert-primary' role='alert'>F&uuml;r die korrekte Funktionsweise dieses Mini-JS-CMS ist ein aktiviertes \
+	JavaScript am Browser erforderlich andernfalls k&ouml;nnen keine Daten angezeigt werden. Weiterhin muss eine Internetverbindung \
+	bestehen, um den CSS-Style zu laden.</div>";
+	
+var bst_003_logikless = 
+	"<div class='alert alert-primary' role='alert'>Das JS-CMS ist so gut wie Logiklos. Die Identifizierung des \
+	einzelnen Beitrags erfolgt anhand der Qualifizierten URL im Browser, der eine entsprechende Ordnerstruktur \
+	zu Grunde liegen muss.</div>";
+	
+var bst_004_templates = 
+	"<div class='alert alert-primary' role='alert'>Templates werden mit Hilfe des Bootstrap CSS Frameworks erzeugt \
+	und verwenden die entsprechenden Klassen des CSS Bootstrap. (<a href='https://getbootstrap.com'>www.GetBootstrap.com</a>)</div>";
