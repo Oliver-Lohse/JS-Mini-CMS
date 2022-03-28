ba3fce4570c4de3f4e168d5e3cd25ce57aec1629
Oliver Lohse <56726226+Oliver-Lohse@users.noreply.github.com>
2022-03-28 15:12:36 +0200

    Update renderer.js

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -1,11 +1,9 @@
-//var res = data.split("---");
-
 // 'template' = DIV Container im Index.HTML wird mit dem
 // Template gefuellt.
 document.getElementById('template').innerHTML = template;
 
 function renderer(template_var, data_var) {
-		document.getElementById(template_var).innerHTML = data_var;
+    document.getElementById(template_var).innerHTML = data_var;
 }
 
 // Inhalt der DATA.JS in das Template schreiben
